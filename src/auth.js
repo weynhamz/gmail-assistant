9243a7e3249408658aee19734790248de11922b6
Weynham Zheng <weynhamz@gmail.com>
2023-03-12 22:54:53 +0800

    Prefer async/await in ES6.

--- src/auth.js
+++ src/auth.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 
 const readline = require('readline');
 const {google} = require('googleapis');
@@ -14,12 +14,11 @@ const SCOPES = [
 const TOKEN_PATH = 'token.json';
 
 /**
- * Get and store new token after prompting for user authorization, and then
- * execute the given callback with the authorized OAuth2 client.
- * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
- * @param {getEventsCallback} callback The callback for the authorized client.
+ * getNewToken.
+ *
+ * @param {Object} oAuth2Client
  */
-function getNewToken(oAuth2Client, callback) {
+async function getNewToken(oAuth2Client) {
   const authUrl = oAuth2Client.generateAuthUrl({
     access_type: 'offline',
     scope: SCOPES,
@@ -32,39 +31,46 @@ function getNewToken(oAuth2Client, callback) {
     output: process.stdout,
   });
 
-  rl.question('Enter the code from that page here: ', (code) => {
-    rl.close();
-    oAuth2Client.getToken(code, (err, token) => {
-      if (err) return console.error('Error retrieving access token', err);
-      oAuth2Client.setCredentials(token);
-      // Store the token to disk for later program executions
-      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-        if (err) return console.error(err);
-        console.log('Token stored to', TOKEN_PATH);
-      });
-      callback(oAuth2Client);
-    });
+  const code = await new Promise((resolve) => {
+    rl.question('Enter the code from that page here: ', resolve);
   });
+
+  rl.close();
+
+  const token = await oAuth2Client.getToken(code);
+
+  // Store the token to disk for later program executions
+  await fs.writeFile(TOKEN_PATH, JSON.stringify(token['tokens']));
+
+  console.log('Token stored to', TOKEN_PATH);
+
+  return token['tokens'];
 }
 
 /**
- * Create an OAuth2 client with the given credentials, and then execute the
- * given callback function.
- * @param {Object} credentials The authorization client credentials.
- * @param {function} callback The callback to call with the authorized client.
+ * authorize.
+ *
+ * @param {Object} credentials
  */
-function authorize(credentials, callback) {
+async function authorize(credentials) {
   // eslint-disable-next-line
   const {client_secret, client_id, redirect_uris} = credentials.installed;
 
   const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
 
-  // Check if we have previously stored a token.
-  fs.readFile(TOKEN_PATH, (err, token) => {
-    if (err) return getNewToken(oAuth2Client, callback);
-    oAuth2Client.setCredentials(JSON.parse(token));
-    callback(oAuth2Client);
-  });
+  let token = null;
+
+  try {
+    // Check if we have previously stored a token.
+    token = await fs.readFile(TOKEN_PATH);
+    token = JSON.parse(token);
+  } catch (e) {
+    token = await getNewToken(oAuth2Client);
+  }
+
+  oAuth2Client.setCredentials(token);
+
+  return oAuth2Client;
 }
 
 exports.authorize = authorize;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ const fs = require('fs');
 const {authorize} = require('./auth.js');
 
 // Load client secrets from a local file.
-fs.readFile('credentials.json', (err, content) => {
+fs.readFile('credentials.json', async (err, credentials) => {
   if (err) return console.log('Error loading client secret file:', err);
 
   // Authorize a client with credentials, then call the Gmail API.
-  authorize(JSON.parse(content), (auth) => {
-    console.log(auth);
-  });
+  const auth = await authorize(JSON.parse(credentials));
+
+  console.log(auth);
 });
