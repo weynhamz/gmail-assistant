2500c137fae645cadce1e378a53360f1337689e3
Weynham Zheng <weynhamz@gmail.com>
2023-03-25 21:58:58 +0800

    [SLS][4/4] Create functions with SLS framework.

--- sls/index.js
+++ sls/index.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const {auth_init, auth_callback} = require('./auth');
+const {watchGmailMessages} = require('./pubsub');
+
 exports.http = (request, response) => {
   response.status(200).send('Hello World!');
 };
@@ -7,3 +10,7 @@ exports.http = (request, response) => {
 exports.event = (event, callback) => {
   callback();
 };
+
+exports.auth_init = auth_init;
+exports.auth_callback = auth_callback;
+exports.watchGmailMessages = watchGmailMessages;
